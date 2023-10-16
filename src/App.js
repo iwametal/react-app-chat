517084fd764af58c1278be5f85555da6dbd0485d
岩 <od.leonardo01@gmail.com>
2023-10-16 00:23:10 +0000

    update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>🪬🦊</h1>
         <SignOut />
       </header>
 
@@ -53,7 +53,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>Bem vindo ao chat da Putaria</p>
     </>
   )
 
@@ -103,7 +103,7 @@ function ChatRoom() {
 
     <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="メッセージを入力" />
 
       <button type="submit" disabled={!formValue}>🕊️</button>
 
